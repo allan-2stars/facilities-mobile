3da8bdf449b4f576eb0804bd3f657c6711e7429b
Allan Wang <wang.allan@outlook.com>
2019-03-20 11:37:35 +1100

    add and apply helmet morgan middleware

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
+
+/// --------------------------
+///
+/// ---    Middle Wares    ---
+///
+// Helmet helps you secure your Express apps by setting various HTTP headers
+app.use(helmet());
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  {
+    flags: 'a'
+  }
+);
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+
+// Body parser to parse front-end form body
+// body name match the name of body.name
+// add middleware here
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/// ------------------------------
+/// ------------------------------
+
+app.get('/', function(req, res, next) {
+  res.send('Testing!');
+});
 
 // DB config
 const db = require('./config/keys').mongoURI;
